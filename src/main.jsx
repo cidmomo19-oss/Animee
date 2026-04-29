46e823ce6834cf5e5b8e37584756608627a32f0e
Okienak <cidmomo19@gmail.com>
2026-04-29 10:49:24 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import Layout from './Layout.jsx';
-import App from './App.jsx'; // Ini jadi Halaman Home
-import Admin from './Admin.jsx';
-import Search from './pages/Search.jsx';
-import Detail from './pages/Detail.jsx';
-import Watch from './pages/Watch.jsx';
-import Genre from './pages/Genre.jsx';
-import List from './pages/List.jsx';
-import Settings from './pages/Settings.jsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App.jsx'
+import Admin from './Admin.jsx'
+import SearchPage from './SearchPage.jsx' // Halaman baru
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        {/* Rute dengan BottomNav */}
-        <Route path="/" element={<Layout />}>
-          <Route index element={<App />} />
-          <Route path="genre" element={<Genre />} />
-          <Route path="list" element={<List />} />
-          <Route path="settings" element={<Settings />} />
-        </Route>
-        
-        {/* Rute tanpa BottomNav */}
-        <Route path="/search" element={<Search />} />
-        <Route path="/anime/:animeId" element={<Detail />} />
-        <Route path="/watch/:episodeId" element={<Watch />} />
+        <Route path="/" element={<App />} />
+        <Route path="/search" element={<SearchPage />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
